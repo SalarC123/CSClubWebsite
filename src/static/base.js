ebb00a41d61949be76f45d0530750963a648a537
SalarC123 <68439561+SalarC123@users.noreply.github.com>
2021-08-01 18:57:23 -0500

    Browser support (#20)
    * add green underline for active page
    
    * update todo list
    
    * changed active css class to active-page to avoid conflicts
    
    * change transition target
    
    * changed clamp to max and min to improve browser support
    
    Co-authored-by: Salar Cheema <salar@Salars-MacBook-Air.local>

--- src/static/base.js
+++ src/static/base.js
@@ -2,6 +2,6 @@ const hamburgerIcon = document.querySelector(".hamburger-icon")
 const hamburgerDropdown = document.querySelector(".hamburger-dropdown")
 
 function mobileMenuActivate() {
-    hamburgerDropdown.style.transition = "margin-top 0.5s ease"
+    hamburgerDropdown.style.transition = "all 0.5s ease"
     hamburgerDropdown.classList.toggle("active")
 }
